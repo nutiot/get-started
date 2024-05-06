5afeba73469784792faf5ecbdc29ccef6c4aa4dd
Siwei Xu <xusiwei1236@qq.com>
2024-05-06 23:06:49 +0800

    Update fetch_code.sh

--- fetch_code.sh
+++ fetch_code.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-REPO_LIST_HDR=mini_repo_hdr.txt
-REPO_LIST_SRC=mini_repo_src.txt
+REPO_LIST_RV32=mini_repo_qemu_riscv32.txt
+REPO_LIST_W800=mini_repo_hihope_w800.txt
 REPO_LIST_ALL=mini_repo_all.txt
 
-cat $REPO_LIST_HDR $REPO_LIST_SRC > $REPO_LIST_ALL
+cat $REPO_LIST_RV32 $REPO_LIST_W800 | sort | uniq > $REPO_LIST_ALL
 
 num=1
 total=`wc -l $REPO_LIST_ALL`
 
+repo init -u git@gitee.com:openharmony/manifest.git -b OpenHarmony-4.1-Release --no-repo-verify
+
+echo "Pull git repos ..."
 for i in `cat $REPO_LIST_ALL`; do
     echo "[$num/$total] Fetch $i ..."
     repo sync -c $i
