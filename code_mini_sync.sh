1a766c1246fb9303ddf34d0a27f758773050fc54
Siwei Xu <xusiwei1236@qq.com>
2024-05-15 20:30:59 +0800

    Update repo list

--- code_mini_sync.sh
+++ code_mini_sync.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 THIS_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
-REPO_LIST_RV32=$THIS_DIR/mini_qemu_riscv32.txt
-REPO_LIST_W800=$THIS_DIR/mini_hihope_w800.txt
-REPO_LIST_ALL=mini_repo_all.txt
 
-cat $REPO_LIST_RV32 $REPO_LIST_W800 | sort | uniq > $REPO_LIST_ALL
+if [ "$1" == "" ]; then
+    echo "Usage: $0 repo_list.txt"
+    exit 1
+fi
 
-echo $THIS_DIR
+REPO_LIST=$1
 
 num=1
-total=`wc -l $REPO_LIST_ALL`
+total=`wc -l $REPO_LIST`
 
 repo init -u git@gitee.com:openharmony/manifest.git -b refs/tags/OpenHarmony-v4.1-Release --no-repo-verify
 
 echo "Pull git repos ..."
-for i in `cat $REPO_LIST_ALL`; do
+for i in `cat $REPO_LIST`; do
     echo "[$num/$total] Fetch $i ..."
     repo sync -c $i
     num=`expr $num + 1`
